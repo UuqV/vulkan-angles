a3f9c4be5e31b1acaadce9cfb824001f66cbb968
Kathryn Lovell <krlovell017@gmail.com>
2025-12-29 18:59:19 -0500

    compile

--- triangle/src/main.cpp
+++ triangle/src/main.cpp
@@ -11,6 +11,7 @@
 #include <cstdint>
 #include <limits>
 #include <algorithm>
+#include <fstream>
 
 class HelloTriangleApplication
 {
@@ -101,6 +102,28 @@ private:
 
     void createGraphicsPipeline()
     {
+        auto vertShaderCode = readFile("shaders/vert.spv");
+        auto fragShaderCode = readFile("shaders/frag.spv");
+    }
+
+    static std::vector<char> readFile(const std::string &filename)
+    {
+        std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+        if (!file.is_open())
+        {
+            throw std::runtime_error("failed to open file!");
+        }
+
+        size_t fileSize = (size_t)file.tellg();
+        std::vector<char> buffer(fileSize);
+
+        file.seekg(0);
+        file.read(buffer.data(), fileSize);
+
+        file.close();
+
+        return buffer;
     }
 
     void createImageViews()
--- triangle/src/shaders/compile.sh
+++ triangle/src/shaders/compile.sh
@@ -1,1 +1,2 @@
-/usr/local/bin/glslc shader.vert -o vert.spv
+/usr/local/bin/glslc shader.vert -o ../../build/vert.spv
+/usr/local/bin/glslc shader.frag -o ../../build/frag.spv
