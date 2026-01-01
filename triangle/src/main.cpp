346b9cd470d4aed913afb613faf3a147035a9c4e
Kathryn Lovell <krlovell017@gmail.com>
2026-01-01 16:11:56 -0500

    []

--- triangle/src/main.cpp
+++ triangle/src/main.cpp
@@ -132,7 +132,7 @@ private:
 
         VkSubmitInfo submitInfo{};
         submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-        VkSemaphore waitSemaphores = {imageAvailableSemaphore};
+        VkSemaphore waitSemaphores[] = {imageAvailableSemaphore};
         VkPipelineStageFlags waitStages[] = {VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT};
         submitInfo.waitSemaphoreCount = 1;
         submitInfo.pWaitSemaphores = waitSemaphores;
