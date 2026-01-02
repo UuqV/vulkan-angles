be0f0ab3d06157c2304118642211e19f1dd95214
Kathryn Lovell <krlovell017@gmail.com>
2026-01-02 17:40:06 -0500

    fix compile script

--- triangle/compile.sh
+++ triangle/compile.sh
@@ -1,2 +1,2 @@
-/usr/local/bin/glslc ./src/shaders/shader.vert -o ./build/vert.spv
-/usr/local/bin/glslc ./src/shaders/shader.frag -o ./build/frag.spv
+/usr/local/bin/glslc ./src/shaders/shader.vert -o ./build/bin/vert.spv
+/usr/local/bin/glslc ./src/shaders/shader.frag -o ./build/bin/frag.spv
