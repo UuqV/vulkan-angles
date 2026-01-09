60771e291fca80c84890514961cb3a6749541f2c
Kathryn Lovell <krlovell017@gmail.com>
2026-01-09 17:24:59 -0500

    embiggen

--- triangle/src/main.cpp
+++ triangle/src/main.cpp
@@ -151,10 +151,10 @@ private:
         0, 1, 2, 2, 3, 0};
 
     const std::vector<Vertex> vertices = {
-        {{-0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-        {{0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}},
-        {{0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-        {{-0.5f, 0.5f}, {1.0f, 1.0f, 1.0f}}};
+        {{-1.0f, -1.0f}, {1.0f, 0.0f, 0.0f}},
+        {{1.0f, -1.0f}, {0.0f, 1.0f, 0.0f}},
+        {{1.0f, 1.0f}, {0.0f, 0.0f, 1.0f}},
+        {{-1.0f, 1.0f}, {1.0f, 1.0f, 1.0f}}};
 
     const std::vector<const char *>
         deviceExtensions = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
