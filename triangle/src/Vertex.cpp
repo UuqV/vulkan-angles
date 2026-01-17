05e1895d6e7dfc650c335aa73515f391a1cf6c83
Kathryn Lovell <krlovell017@gmail.com>
2026-01-17 13:03:43 -0500

    reduce imports

--- triangle/src/Vertex.cpp
+++ triangle/src/Vertex.cpp
@@ -6,21 +6,8 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
-#include <chrono>
-
-#include <iostream>
-#include <stdexcept>
-#include <cstdlib>
 #include <vector>
-#include <cstring>
-#include <optional>
-#include <set>
-#include <cstdint>
-#include <limits>
-#include <algorithm>
-#include <fstream>
 #include <array>
-#include "stb_image.h"
 
 struct Vertex
 {
