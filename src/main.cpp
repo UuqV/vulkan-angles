88198ab91c9d6b6dbbb7f71a67d2edeb92631be8
Kathryn Lovell <krlovell017@gmail.com>
2026-02-04 16:48:50 -0500

    see it

--- src/main.cpp
+++ src/main.cpp
@@ -155,7 +155,7 @@ private:
         vkBeginCommandBuffer(commandBuffers[currentFrame], &beginInfo);
 
         // Camera position
-        glm::vec3 camPos = glm::vec3(2.0f, 2.0f, 1.0f);
+        glm::vec3 camPos = glm::vec3(0.0f, 0.0f, 3.0f);
 
         // 90° FOV projection for cubemap faces
         glm::mat4 proj = glm::perspective(glm::radians(90.0f), 1.0f, 0.1f, 100.0f);
